ec44463131d01af75e4fbff8ecddade745fa8824
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-17 09:15:47 -0300

    Create Booking History Model

---
+++ HotelBookingAPI/Models/BookingHistory.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBookingAPI.Models;
+
+public class BookingHistory
+{
+    public Guid Id { get; set; }
+    public Guid TravelerId { get; set; }
+    public Traveler? Traveler { get; set; }
+    public List<Booking>? Bookings { get; set; }
+}
