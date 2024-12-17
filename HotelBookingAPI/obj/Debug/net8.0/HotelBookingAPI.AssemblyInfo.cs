a550f185b7059f4a2e97a2d529e285e4c7153b19
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-17 09:20:07 -0300

    Cleanup of build files and cache in the HotelBookingSystem project

--- HotelBookingAPI/obj/Debug/net8.0/HotelBookingAPI.AssemblyInfo.cs
+++ HotelBookingAPI/obj/Debug/net8.0/HotelBookingAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b601ec43699ec211c3d9296b1bd3a4154af4c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838af46f562c4550e1e0dbed308aca958faa0b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
