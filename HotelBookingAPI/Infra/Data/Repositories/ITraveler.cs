ecd7383d127feb44622cfd6e36e1f83b58b1e40f
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-20 07:51:14 -0300

    feat: Create Traveler interface

---
+++ HotelBookingAPI/Infra/Data/Repositories/ITraveler.cs
@@ -0,0 +1,9 @@
+﻿using HotelBookingAPI.Dtos;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Infra.Data.Repositories;
+
+public interface ITraveler
+{
+    Task<ServiceResultDto<CreateTravelerDto>> CreateTraveler(CreateTravelerDto createTravelerDto, string userId);
+}
