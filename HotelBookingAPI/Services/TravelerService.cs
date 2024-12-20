bd224d5ff344e41397f8bcc894dfe7ba34e51689
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-20 07:52:00 -0300

    Create Traveler service

---
+++ HotelBookingAPI/Services/TravelerService.cs
@@ -0,0 +1,36 @@
+﻿using HotelBookingAPI.Dtos;
+using HotelBookingAPI.Infra.Data;
+using HotelBookingAPI.Infra.Data.Repositories;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Services;
+
+public class TravelerService: ITraveler
+{
+    private readonly AppDbContext _dbContext;
+
+    public TravelerService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<ServiceResultDto<CreateTravelerDto>> CreateTraveler(CreateTravelerDto createTravelerDto, string userId)
+    {
+        var traveler = new Traveler(createTravelerDto);
+        traveler.UserId = userId;
+        traveler.CreatedBy = userId;
+        traveler.CreatedOn = DateTime.Now;
+
+        if(!traveler.IsValid)
+        {
+            var result = ServiceResultDto<CreateTravelerDto>.Fail("Não foi possível criar o viajante",traveler.Notifications.Select(n => n.Message));
+            return result;
+        }
+
+        await _dbContext.Travelers!.AddAsync(traveler);
+        await _dbContext.SaveChangesAsync();
+
+        var successResult = ServiceResultDto<CreateTravelerDto>.SuccessResult(createTravelerDto,"Viajante criado com sucesso.");
+
+        return await Task.FromResult(successResult);
+    }
+}
