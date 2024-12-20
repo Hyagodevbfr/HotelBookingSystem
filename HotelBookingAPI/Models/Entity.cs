829f19fbb2b3aaab6ffae5b505c066f238057c54
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-20 07:36:01 -0300

    feat: add [Key] attribute to Id property in Entity class

--- HotelBookingAPI/Models/Entity.cs
+++ HotelBookingAPI/Models/Entity.cs
@@ -1,9 +1,11 @@
 ﻿using Flunt.Notifications;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelBookingAPI.Models;
 
 public class Entity : Notifiable<Notification>
 {
+    [Key]
     public int Id { get; set; }
     public string CreatedBy { get; set; } = null!;
     public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
