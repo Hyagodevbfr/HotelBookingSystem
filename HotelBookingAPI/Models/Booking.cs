f368ff4d126cab25e4ffd1da095aaa22601ebf6f
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-20 07:32:41 -0300

    fix: update TravelerId type to string and make Traveler nullable

--- HotelBookingAPI/Models/Booking.cs
+++ HotelBookingAPI/Models/Booking.cs
@@ -5,16 +5,14 @@ namespace HotelBookingAPI.Models;
 public class Booking
 {
     public Guid Id { get; set; }
-    public Guid TravelerId { get; set; }
-    public Traveler Traveler { get; set; } = null!;
+    public string TravelerId { get; set; } = string.Empty;
+    public Traveler? Traveler { get; set; }
     public int RoomId { get; set; }
     public Room Room { get; set; } = null!;
     public DateTime CheckInDate { get; set; }
     public DateTime CheckOutDate { get; set; }
     public decimal TotalPrice { get; set; }
     public BookingStatus Status { get; set; }
-
-    
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime EditedAt { get; set; } = DateTime.UtcNow;
     public string CreatedBy { get; set; } = string.Empty;
