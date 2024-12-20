f376fb13720deca6743a1a222bb9ad277d73e678
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-20 07:48:57 -0300

    feat: Create traveler Dto

---
+++ HotelBookingAPI/Dtos/CreateTravelerDto.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelBookingAPI.Dtos;
+
+public class CreateTravelerDto
+{
+    //public string UserId { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+
+    public string EmergencyContact { get; set; } = string.Empty;
+    public string EmergencyContactName { get; set; } = string.Empty;
+
+    public bool HasSpecialNeeds { get; set; }
+    public string? SpecialNeedsDetails { get; set; }
+    public string? DietaryPreferences { get; set; }
+}
