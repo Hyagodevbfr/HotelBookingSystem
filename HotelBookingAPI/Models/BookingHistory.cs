7535fd852a4846601c34a249347c56f37c989d87
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-20 07:34:00 -0300

    fix: initialize TravelerId with empty string instead of null

--- HotelBookingAPI/Models/BookingHistory.cs
+++ HotelBookingAPI/Models/BookingHistory.cs
@@ -3,7 +3,7 @@
 public class BookingHistory
 {
     public Guid Id { get; set; }
-    public string TravelerId { get; set; } = null!;
+    public string TravelerId { get; set; } = string.Empty;
     public Traveler? Traveler { get; set; }
     public List<Booking>? Bookings { get; set; }
 }
