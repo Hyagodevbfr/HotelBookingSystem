a7533c15a76c7986bf80e8d53e347bbd8443adf3
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-23 20:44:11 -0300

    feat: add method to verify duplicate travelers in ITravelerVerifier

--- HotelBookingAPI/Infra/Data/Repositories/ITravelerVerifier.cs
+++ HotelBookingAPI/Infra/Data/Repositories/ITravelerVerifier.cs
@@ -1,5 +1,9 @@
-﻿namespace HotelBookingAPI.Infra.Data.Repositories;
+﻿using HotelBookingAPI.Dtos;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Infra.Data.Repositories;
 
 public interface ITravelerVerifier
 {
+    Task<ServiceResultDto<Traveler>> IsDuplicateTraveler(object travelerDto, string travelId);
 }
