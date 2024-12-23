fae741426ef35a861e845fd40a95cd1c6d13e608
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-23 20:50:05 -0300

    feat: implement traveler duplication check in TravelerVerifierService

--- HotelBookingAPI/Services/TravelerVerifierService.cs
+++ HotelBookingAPI/Services/TravelerVerifierService.cs
@@ -1,7 +1,40 @@
-﻿using HotelBookingAPI.Infra.Data.Repositories;
+﻿using HotelBookingAPI.Dtos;
+using HotelBookingAPI.Infra.Data;
+using HotelBookingAPI.Infra.Data.Repositories;
+using HotelBookingAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelBookingAPI.Services;
 
-public class TravelerVerifierService : ITravelerVerifier
+public class TravelerVerifierService: ITravelerVerifier
 {
+    private readonly AppDbContext _appDb;
+
+    public TravelerVerifierService(AppDbContext appDb)
+    {
+        _appDb = appDb;
+    }
+    public async Task<ServiceResultDto<Traveler>> IsDuplicateTraveler(object travelerDto, string travelerId)
+    {
+        string message = string.Empty;
+        Traveler? traveler = travelerDto switch
+        {
+            CreateTravelerDto createTraveler => await _appDb.Travelers!.FirstOrDefaultAsync(t => t.UserId == travelerId),
+
+            _ => throw new ArgumentException("Não foi possível concluir a validação.")
+        };
+
+        if(traveler is null)
+            return ServiceResultDto<Traveler>.SuccessResult(null,"Não há duplicidade.");
+
+        switch(travelerDto)
+        {
+            case CreateTravelerDto createTraveler:
+                if(traveler.UserId == travelerId)
+                    message += "Úsuário já cadastrado como viajante.";
+            break;
+        }
+
+        return ServiceResultDto<Traveler>.Fail(message.Trim( ));
+    }
 }
