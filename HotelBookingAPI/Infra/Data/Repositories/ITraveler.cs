54062420dd681a4cd3800e00d33cecbc9c89ae63
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-28 18:00:31 -0300

    feat: Add UpdateTraveler method to ITraveler interface

--- HotelBookingAPI/Infra/Data/Repositories/ITraveler.cs
+++ HotelBookingAPI/Infra/Data/Repositories/ITraveler.cs
@@ -6,4 +6,5 @@ namespace HotelBookingAPI.Infra.Data.Repositories;
 public interface ITraveler
 {
     Task<ServiceResultDto<CreateTravelerDto>> CreateTraveler(CreateTravelerDto createTravelerDto, string userId);
+    Task<ServiceResultDto<UpdateTravelerDto>> UpdateTraveler(UpdateTravelerDto updateTravelerDto, string userId);
 }
