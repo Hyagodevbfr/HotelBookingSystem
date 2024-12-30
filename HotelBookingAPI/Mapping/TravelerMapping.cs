290b92593a0bdec5d79f347809e375c5a365e61b
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-30 08:43:42 -0300

    feat: Add traveler mapping

---
+++ HotelBookingAPI/Mapping/TravelerMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HotelBookingAPI.Dtos;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Mapping;
+
+public class TravelerMapping : Profile
+{
+    public TravelerMapping()
+    {
+        CreateMap<Traveler,TravelerDetailDto>( );
+    }
+}
