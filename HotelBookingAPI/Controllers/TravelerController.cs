e404f527086c0bcc4147bbb36a28fe73508a7ab5
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-30 08:45:37 -0300

    feat: Add admin and employee authorization validation and inject user checker dependency

--- HotelBookingAPI/Controllers/TravelerController.cs
+++ HotelBookingAPI/Controllers/TravelerController.cs
@@ -13,11 +13,14 @@ namespace HotelBookingAPI.Controllers;
 public class TravelerController: ControllerBase
 {
     private readonly ITraveler _travelerService;
+    private readonly IUserVerifier _userVerifier;
 
-    public TravelerController(ITraveler travelerService)
+    public TravelerController(ITraveler travelerService, IUserVerifier userVerifier)
     {
         _travelerService = travelerService;
+        _userVerifier = userVerifier;
     }
+    [Authorize(Roles = "Admin, Employee")]
     [HttpPost]
     public async Task<IActionResult> CreateTraveler([FromBody]CreateTravelerDto createTravelerDto)
     {
@@ -34,6 +37,17 @@ public class TravelerController: ControllerBase
             return BadRequest(new { result.Message,result.Errors });
 
         return Ok(new { result.Message,Sucess = result.Success });
+    }
+    [Authorize(Roles = "Admin, Employee")]
+    [HttpGet]
+    public async Task<ActionResult<ServiceResultDto<List<TravelerDetailDto>>>> GetTravelers()
+    {
+        var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier)?.ToString( );
+        if(await _userVerifier.VerifyUserEmployeeOrAdminOrNull(currentUserId!) == false)
+            return Unauthorized(ServiceResultDto<List<TravelerDetailDto>>.Fail("Usuário não autênticado."));
+
+        var result = await _travelerService.GetTravelers();
+        return Ok(result);
     }
     [HttpPatch("{id}")]
     public async Task<ActionResult<ServiceResultDto<UpdateTravelerDto>>> UpdateTraveler([FromBody] UpdateTravelerDto updateTravelerDto, string id) 
