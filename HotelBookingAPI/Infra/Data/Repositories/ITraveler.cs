e6c9409a73ca50b487c2be47fae1f21061a55f31
hyagodevbfr <hyago.sbrito@gmail.com>
2024-12-30 08:46:32 -0300

    feat: Add get travelers method in traveler interface

--- HotelBookingAPI/Infra/Data/Repositories/ITraveler.cs
+++ HotelBookingAPI/Infra/Data/Repositories/ITraveler.cs
@@ -6,5 +6,6 @@ namespace HotelBookingAPI.Infra.Data.Repositories;
 public interface ITraveler
 {
     Task<ServiceResultDto<CreateTravelerDto>> CreateTraveler(CreateTravelerDto createTravelerDto, string userId);
+    Task<ServiceResultDto<List<TravelerDetailDto>>> GetTravelers();
     Task<ServiceResultDto<UpdateTravelerDto>> UpdateTraveler(UpdateTravelerDto updateTravelerDto, string userId, string authenticatedUser);
 }
