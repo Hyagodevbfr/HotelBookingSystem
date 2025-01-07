f85e1d83a28c7dc2ddfe89dc5219d39fad2c0e6d
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-07 06:08:35 -0300

    feat: add get details available room to room interface

--- HotelBookingAPI/Infra/Data/Repositories/IRoom.cs
+++ HotelBookingAPI/Infra/Data/Repositories/IRoom.cs
@@ -13,5 +13,5 @@ public interface IRoom
 
     //Search
     Task<ServiceResultDto<IEnumerable<RoomSearchResponse>>> GetAvaliableRooms([FromBody]RoomSearchRequest searchRequest);
-    Task<ServiceResultDto<DetailsAvailableRoom>> GetDetailsAvailableRoom(int id, [FromQuery] string queries);
+    Task<ServiceResultDto<DetailsAvailableRoom>> GetDetailsAvailableRoom(int id, string userId, [FromQuery] string queries);
 }
