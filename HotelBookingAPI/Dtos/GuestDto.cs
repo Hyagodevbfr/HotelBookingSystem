dcb0c2e541e794071db5366fae9085262f449e4d
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-07 09:42:55 -0300

    feat: Create guest Dto

---
+++ HotelBookingAPI/Dtos/GuestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.Dtos;
+
+public class GuestDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? NationalId { get; set; } = string.Empty;
+    public string RegistrationId { get; set; } = string.Empty;
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+
+    public bool HasSpecialNeeds { get; set; }
+    public string? SpecialNeedsDetails { get; set; }
+    public string? DietaryPreferences { get; set; }
+}
