ee4e0606a9c4ba3a3ae32630890b480a40bef0c3
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-09 08:50:12 -0300

    feat: create booking service

---
+++ HotelBookingAPI/Services/BookingService.cs
@@ -0,0 +1,12 @@
+﻿using HotelBookingAPI.Dtos;
+using HotelBookingAPI.Infra.Data.Repositories;
+
+namespace HotelBookingAPI.Services;
+
+public class BookingService: IBooking
+{
+    public Task<ServiceResultDto<CreateBookingDto>> CreateBooking(CreateBookingDto createBookingDto)
+    {
+        throw new NotImplementedException( );
+    }
+}
