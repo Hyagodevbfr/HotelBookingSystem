2e072fe35672fa8884116ef988f6c7ffa4729729
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-09 08:49:19 -0300

    feat: create createbooking dto

---
+++ HotelBookingAPI/Dtos/CreateBookingDto.cs
@@ -0,0 +1,15 @@
+﻿using HotelBookingAPI.Enums;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Dtos;
+
+public class CreateBookingDto
+{
+    public string TravelerId { get; set; } = string.Empty;
+    public List<GuestBooking>? GuestBookings { get; set; }
+    public int RoomId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public decimal TotalPrice { get; set; }
+    public BookingStatus Status { get; set; }
+}
