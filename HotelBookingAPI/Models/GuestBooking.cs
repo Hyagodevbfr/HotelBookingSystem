2218ac9bae27c3d6b97c9e6e64e629251f0b4afc
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-09 08:49:43 -0300

    feat: create guest booking model

---
+++ HotelBookingAPI/Models/GuestBooking.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelBookingAPI.Models;
+
+public class GuestBooking
+{
+    public int BookingId { get; set; }
+    public Booking Booking { get; set; } = null!;
+
+    public int GuestId { get; set; }
+    public Guest Guest { get; set; } = null!;
+}
