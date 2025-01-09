616240d30595d27b829aeef005fb7a4abc8dcb0a
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-09 08:43:50 -0300

    refactor: alter name details available room dto

--- HotelBookingAPI/Mapping/RoomMapping.cs
+++ HotelBookingAPI/Mapping/RoomMapping.cs
@@ -9,6 +9,6 @@ public class RoomMapping : Profile
     public RoomMapping()
     {
         CreateMap<Room,RoomDetailDto>( );
-        CreateMap<Room,DetailsAvailableRoom>( );
+        CreateMap<Room,DetailsAvailableRoomDto>( );
     }
 }
