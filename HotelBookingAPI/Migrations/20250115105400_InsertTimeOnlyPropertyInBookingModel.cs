83a7e3310ae11cae29faa93838fbe2e58c3de939
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-15 20:06:03 -0300

    feat: Create new migration

---
+++ HotelBookingAPI/Migrations/20250115105400_InsertTimeOnlyPropertyInBookingModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertTimeOnlyPropertyInBookingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CheckInTime",
+                table: "Bookings",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CheckOutTime",
+                table: "Bookings",
+                type: "time",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckInTime",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutTime",
+                table: "Bookings");
+        }
+    }
+}
