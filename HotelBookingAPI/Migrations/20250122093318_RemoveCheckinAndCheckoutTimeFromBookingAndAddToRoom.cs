821e812c7cc872c41b03f7914755b2685cb3532d
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-22 14:04:17 -0300

    feat: Add new Migration

---
+++ HotelBookingAPI/Migrations/20250122093318_RemoveCheckinAndCheckoutTimeFromBookingAndAddToRoom.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBookingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCheckinAndCheckoutTimeFromBookingAndAddToRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckInTime",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutTime",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CheckInTime",
+                table: "Rooms",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CheckOutTime",
+                table: "Rooms",
+                type: "time",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckInTime",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutTime",
+                table: "Rooms");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CheckInTime",
+                table: "Bookings",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CheckOutTime",
+                table: "Bookings",
+                type: "time",
+                nullable: true);
+        }
+    }
+}
