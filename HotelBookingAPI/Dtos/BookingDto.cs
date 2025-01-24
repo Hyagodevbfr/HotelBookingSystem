d45f0f48704a5d540c41dc83960098be141df19b
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-24 20:47:52 -0300

    feat: Add booking dto

---
+++ HotelBookingAPI/Dtos/BookingDto.cs
@@ -0,0 +1,25 @@
+﻿using HotelBookingAPI.Enums;
+using HotelBookingAPI.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.Dtos
+{
+    public class BookingDto
+    {
+        public int Id { get; set; }
+        public string TravelerId { get; set; } = string.Empty;
+        public string TravelerFullName { get; set; } = string.Empty;
+        public string TravelerNationalId { get; set; } = string.Empty;
+        public Traveler? Traveler { get; set; }
+        public List<GuestDto>? Guests { get; set; }
+        public int RoomId { get; set; }
+        public Room Room { get; set; } = null!;
+        [DataType(DataType.Date)]
+        public DateTime CheckInDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CheckOutDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public BookingStatus Status { get; set; }
+    }
+}
