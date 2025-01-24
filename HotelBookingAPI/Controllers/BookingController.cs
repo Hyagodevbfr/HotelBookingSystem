0b956decb285a23d73fb2d029a793d94710aa34e
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-24 20:48:51 -0300

    feat: Add get all bookings method

--- HotelBookingAPI/Controllers/BookingController.cs
+++ HotelBookingAPI/Controllers/BookingController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelBookingAPI.Dtos;
 using HotelBookingAPI.Infra.Data;
 using HotelBookingAPI.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,11 +14,13 @@ public class BookingController: ControllerBase
 {
     private readonly AppDbContext _dbContext;
     private readonly IBooking _bookingService;
+    private readonly IUserVerifier _userVerifier;
 
-    public BookingController(AppDbContext dbContext, IBooking bookingService)
+    public BookingController(AppDbContext dbContext, IBooking bookingService, IUserVerifier userVerifier)
     {
         _dbContext = dbContext;
         _bookingService = bookingService;
+        _userVerifier = userVerifier;
     }
     [HttpPost]
     public async Task<ActionResult<ServiceResultDto<CreateBookingDto>>> CreateBooking([FromBody] BookingRequest bookingRequest)
@@ -30,6 +33,21 @@ public class BookingController: ControllerBase
         var result = await _bookingService.CreateBooking(bookingRequest);
         if(!result.Success)
             return BadRequest(result.Errors);
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Admin, Employee")]
+    public async Task<ActionResult<ServiceResultDto<List<BookingDto>>>> GetAllBookings()
+    {
+        var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier)?.ToString( );
+        if(await _userVerifier.VerifyUserEmployeeOrAdminOrNull(currentUserId!) == false)
+            return Unauthorized(ServiceResultDto<IEnumerable<UserDetailDto>>.Fail("Usuário não autênticado."));
+
+        var result = await _bookingService.GetAllBookings();
+        if(!result.Success)
+            return NotFound();
 
         return Ok(result);
     }
