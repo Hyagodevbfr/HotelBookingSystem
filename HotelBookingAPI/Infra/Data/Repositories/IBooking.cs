248ad450ad0c043c952ba0c0af2cfec9ab54868b
hyagodevbfr <hyago.sbrito@gmail.com>
2025-01-28 20:30:44 -0300

    feat: Add update booking status to booking interface

--- HotelBookingAPI/Infra/Data/Repositories/IBooking.cs
+++ HotelBookingAPI/Infra/Data/Repositories/IBooking.cs
@@ -7,5 +7,6 @@ public interface IBooking
     Task<ServiceResultDto<CreateBookingDto>> CreateBooking(BookingRequest bookingRequest);
     Task<ServiceResultDto<List<BookingDto>>> GetAllBookings();
     Task<ServiceResultDto<BookingDto>> GetBooking(int id);
+    Task<ServiceResultDto<string>> UpdateBookingStatus(int id, string userId,BookingStatusDto bookingStatus);
 
 }
