100ccbb90586c48aebfcdda8acf447a74be9073f
hyagodevbfr <hyago.sbrito@gmail.com>
2025-02-04 21:41:49 -0300

    feat: Add checkin method in booking service

--- HotelBookingAPI/Services/BookingService.cs
+++ HotelBookingAPI/Services/BookingService.cs
@@ -5,6 +5,7 @@ using HotelBookingAPI.Infra.Data;
 using HotelBookingAPI.Infra.Data.Repositories;
 using HotelBookingAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HotelBookingAPI.Services;
 
@@ -205,7 +206,7 @@ public class BookingService: IBooking
 
     public async Task<ServiceResultDto<BookingDto>> GetBooking(int id)
     {
-        var booking = await _dbContext.Bookings!.Where(b => b.Id == id).Include(b => b.Traveler).ThenInclude(t => t!.User).Include(b => b.GuestBookings)!.ThenInclude(gb => gb.Guest).FirstOrDefaultAsync();
+        var booking = await _dbContext.Bookings!.Where(b => b.Id == id).Include(b => b.Traveler).ThenInclude(t => t!.User).Include(b => b.GuestBookings)!.ThenInclude(gb => gb.Guest).FirstOrDefaultAsync( );
         if(booking is null)
             return ServiceResultDto<BookingDto>.NullContent("Reserva não localizada.");
 
@@ -232,7 +233,7 @@ public class BookingService: IBooking
             Status = booking.Status
         };
 
-        var successReult = ServiceResultDto<BookingDto>.SuccessResult(bookingDto, "Reserva localizada." );
+        var successReult = ServiceResultDto<BookingDto>.SuccessResult(bookingDto,"Reserva localizada.");
 
         return successReult;
     }
@@ -302,14 +303,14 @@ public class BookingService: IBooking
             TotalPrice = booking.TotalPrice,
             Status = booking.Status
         }).ToList( );
-        if(!bookingsDto.Any())
+        if(!bookingsDto.Any( ))
             return ServiceResultDto<List<BookingDto>>.NullContent($"Não há quartos com o status {bookingStatus}");
 
         var successResult = ServiceResultDto<List<BookingDto>>.SuccessResult(bookingsDto,"Quartos localizados.");
         return successResult;
     }
 
-    public async Task<ServiceResultDto<string>> UpdateBookingStatus(int id, string userId,BookingStatus bookingStatus)
+    public async Task<ServiceResultDto<string>> UpdateBookingStatus(int id,string userId,BookingStatus bookingStatus)
     {
         var booking = await _dbContext.Bookings!.FirstOrDefaultAsync(b => b.Id == id);
         if(booking is null)
@@ -322,18 +323,78 @@ public class BookingService: IBooking
             if(room is not null)
             {
                 room.RoomsQuantity += 1;
-                booking.TotalPrice = Math.Abs(booking.TotalPrice) * - 1;
-                _dbContext.SaveChanges();
+                booking.TotalPrice = Math.Abs(booking.TotalPrice) * -1;
+                _dbContext.SaveChanges( );
             }
             else
-                return ServiceResultDto<string>.NullContent("Quarto não localizado.");           
+                return ServiceResultDto<string>.NullContent("Quarto não localizado.");
+        }
+        if(booking.Status == BookingStatus.CheckoutCompleted || booking.Status == BookingStatus.CheckinCompleted)
+        {
+            return ServiceResultDto<string>.Fail("Não é possível alterar para esse status nessa página.");
         }
         booking.EditedAt = DateTime.Now;
         booking.EditedBy = userId;
         await _dbContext.SaveChangesAsync( );
 
-        var successResult = ServiceResultDto<string>.SuccessResult("O status da reserva foi atualizado com sucesso.", "Status atualizado.");
+        var successResult = ServiceResultDto<string>.SuccessResult("O status da reserva foi atualizado com sucesso.","Status atualizado.");
 
         return successResult;
     }
+
+    public async Task<ServiceResultDto<string>> Checkin(int bookingId, bool confirmAction = false)
+    {
+        var booking = await _dbContext.Bookings!.FirstOrDefaultAsync(b => b.Id == bookingId);
+        if(booking is null)
+            return ServiceResultDto<string>.NullContent("Reserva não localizada.");
+
+        if(booking.Status != BookingStatus.Confirmed)
+            return ServiceResultDto<string>.Fail("o status da reserva não permite realizar o checkin.");
+
+        DateOnly concatenatedCheckInDate = DateOnly.FromDateTime(DateTime.Now);
+        TimeOnly concatenatedCheckInTime = TimeOnly.FromDateTime(DateTime.Now);
+
+        DateTime ajustedCheckinDate = new DateTime(
+            concatenatedCheckInDate.Year,
+            concatenatedCheckInDate.Month,
+            concatenatedCheckInDate.Day,
+            concatenatedCheckInTime.Hour,
+            concatenatedCheckInTime.Minute,
+            concatenatedCheckInTime.Second,
+            concatenatedCheckInTime.Millisecond
+        );
+        TimeSpan diff = booking.CheckInDate - ajustedCheckinDate;
+        if( diff.TotalHours > 3 )
+        {
+            if(confirmAction == false)
+            {
+                double totalHours = diff.TotalHours;
+                int days = (int)(totalHours / 24);
+                int hours = (int)(totalHours % 24);
+                int minutes = diff.Minutes;
+
+                string timeRemaining = days > 0
+                    ? $"{days} dias, {hours} horas e {minutes} minutos"
+                    : hours > 0
+                        ? $"{hours} horas e {minutes} minutos"
+                        : $"{minutes} minutos";
+                return ServiceResultDto<string>.Fail($"Check-in só permitido 3 horas antes do horário marcado. Aguarde mais {timeRemaining}.");
+            }
+            
+            booking.Status = BookingStatus.CheckinCompleted;
+            _dbContext.SaveChanges();
+        }
+        else if(confirmAction == false && diff.TotalHours > 0)
+        {
+                return ServiceResultDto<string>.Fail($"Tentativa de checkin antes de horario marcado sem confirmação. {diff}");
+        }
+       
+
+        booking.Status = BookingStatus.CheckinCompleted;
+        _dbContext.SaveChanges( );
+
+        return ServiceResultDto<string>.SuccessResult("Checkin realizado com sucesso.","Checkin realizado.");
+    }
 }
+
+
