b0ef1a0844d29bcca7d8e07e5fe05cc1786f7851
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-10 14:09:45 -0300

    Feat: Create queue config class

---
+++ HotelBookingAPI/Infra/Data/QueueConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelBookingAPI.Infra.Data;
+
+public class QueueConfig
+{
+    public const string QueueName = "booking_queue";
+    public const string ExchangeName = "hotel_exchange";
+    public const string HostName = "localhost";
+}
