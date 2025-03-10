43ef1ba9f4dfd0c903291902296b71feb6353903
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-10 14:09:11 -0300

    Feat: Create Xml Helper

---
+++ HotelBookingAPI/Helpers/XmlHelper.cs
@@ -0,0 +1,20 @@
+﻿using HotelBookingAPI.Dtos;
+using Shared;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Xsl;
+using static Shared.BookingSharedDto;
+
+namespace HotelBookingAPI.Messenger.XmlHelpers;
+
+public class XmlHelper
+{
+    public static string GenerateXml(BookingShared booking)
+    {
+        var serializer = new XmlSerializer(typeof(BookingShared));
+
+        using var stringWriter = new StringWriter();
+        serializer.Serialize(stringWriter, booking);
+        return stringWriter.ToString();
+    }
+}
