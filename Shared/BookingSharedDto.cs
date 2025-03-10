a22a2caaafba8c26243fde667e849e52a74bd8bf
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-10 14:08:10 -0300

    Feat: create project to share information between other projects

---
+++ Shared/BookingSharedDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared;
+
+public class BookingSharedDto
+{
+    public BookingSharedDto(){}
+    public class BookingShared
+    {
+        public int BookingId { get; set; }
+        public string TravelerFullName { get; set; } = string.Empty;
+        public string TravelerEmail { get; set; } = string.Empty;
+        public string TravelerNationalId { get; set; } = string.Empty;
+
+        //Room Info
+        public string? TypeRoom { get; set; }
+        public string? RoomName { get; set; }
+        public string CheckIn { get; set; } = null!;
+        public string CheckOut { get; set; } = null!;
+        public string TotalPrice { get; set; } = null!;
+        public string Status { get; set; } = null!;
+    }
+}
