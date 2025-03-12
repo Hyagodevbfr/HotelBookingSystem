c0f0174b79903129249c39462dcc361902120ef4
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-12 08:45:18 -0300

    Feat: Isolate queue configurations

---
+++ Shared/QueueConfiguation.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared
+{
+    public class QueueConfiguation
+    {
+        public const string QueueName = "booking_queue";
+        public const string ExchangeName = "hotel_exchange";
+        public const string HostName = "localhost";
+
+        public const string NewBookingRouting = "booking.new";
+        public const string StatusUpdateRouting = "booking.status_update";
+    }
+}
