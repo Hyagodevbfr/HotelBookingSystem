05dd52072df435f1f1ae66ed7a3e8d2f5803d85c
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-12 10:14:51 -0300

    Refactor: Remove unitized queue config class

--- HotelBookingAPI/Infra/Data/QueueConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HotelBookingAPI.Infra.Data;
-
-public class QueueConfig
-{
-    public const string QueueName = "booking_queue";
-    public const string ExchangeName = "hotel_exchange";
-    public const string HostName = "localhost";
-}
