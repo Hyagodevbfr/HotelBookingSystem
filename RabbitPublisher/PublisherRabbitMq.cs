38f66e1a98a085e2fe0e12901f256daccfa4ef93
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-12 10:10:37 -0300

    Feat: Changes the ways of using the publisher and creates the queue for sending emails about status updates

--- RabbitPublisher/PublisherRabbitMq.cs
+++ RabbitPublisher/PublisherRabbitMq.cs
@@ -2,18 +2,18 @@
 using System.Text;
 using RabbitMQ.Client;
 using static Shared.BookingSharedDto;
+using static Shared.QueueConfiguation;
+
 
 namespace RabbitPublisher;
 
 public class PublisherRabbitMq
 {
     private readonly string _hostName;
-
     public PublisherRabbitMq(string hostName = "localhost")
     {
         _hostName = hostName;
     }
-
     public async Task SendBookingAsync(BookingShared booking)
     {
         var factory = new ConnectionFactory { HostName = _hostName };
@@ -22,16 +22,59 @@ public class PublisherRabbitMq
         using var channel = await connection.CreateChannelAsync( );
 
         await channel.ExchangeDeclareAsync("booking_exchange",ExchangeType.Topic);
+        try
+        {
+            Console.WriteLine($"[x] Waiting message to sent");
 
-        Console.WriteLine($"[x] Waiting message to sent");
+            var bookingJson = JsonSerializer.Serialize(booking);
+            var body = Encoding.UTF8.GetBytes(bookingJson);
 
-        var bookingJson = JsonSerializer.Serialize(booking);
-        var body = Encoding.UTF8.GetBytes(bookingJson);
+            await channel.BasicPublishAsync(
+                exchange: ExchangeName,
+                routingKey: NewBookingRouting,
+                body: body
+                );
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Erro ao processar envio de email: {ex.Message}");
+        }
+        finally
+        {
+            if(channel != null && channel.IsOpen)
+                await channel.CloseAsync( );
+        }
+    }
 
-        await channel.BasicPublishAsync(
-            exchange: "booking_exchange",
-            routingKey: "booking.new",
-            body: body
-            );
+    public async Task StatusUpdate(BookingShared booking)
+    {
+        var factory = new ConnectionFactory { HostName = _hostName };
+
+        using var connection = await factory.CreateConnectionAsync( );
+        using var channel = await connection.CreateChannelAsync( );
+
+        await channel.ExchangeDeclareAsync("booking_exchange",ExchangeType.Topic);
+        try
+        {
+            Console.WriteLine($"[x] Waiting for status update message to send");
+
+            var bookingJson = JsonSerializer.Serialize(booking);
+            var body = Encoding.UTF8.GetBytes(bookingJson);
+
+            await channel.BasicPublishAsync(
+                exchange: ExchangeName,
+                routingKey: StatusUpdateRouting,
+                body: body
+                );
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Erro ao processar envio de email: {ex.Message}");
+        }
+        finally
+        {
+            if(channel != null && channel.IsOpen)
+                await channel.CloseAsync( );
+        }
     }
 }
