9f40e2131f03a7734c970455ec86690d104ab5a9
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-19 12:53:55 -0300

    feat: convert date time and decimal properties to string

--- HotelBookingAPI/Dtos/CreateBookingDto.cs
+++ HotelBookingAPI/Dtos/CreateBookingDto.cs
@@ -1,17 +1,20 @@
 ﻿using HotelBookingAPI.Enums;
 using HotelBookingAPI.Models;
+using System.Text.Json.Serialization;
 
 namespace HotelBookingAPI.Dtos;
 
 public class CreateBookingDto
 {
     public string TravelerId { get; set; } = string.Empty;
-    public List<GuestBooking>? GuestBookings { get; set; }
+    public List<GuestBookingDto>? GuestBookings { get; set; }
     public int RoomId { get; set; }
-    public DateTime CheckInDate { get; set; }
-    public DateTime CheckOutDate { get; set; }
+    public string CheckInDate { get; set; } = string.Empty;
+    public string CheckOutDate { get; set; } = string.Empty;
     public TimeOnly? CheckInTime { get; set; }
     public TimeOnly? CheckOutTime { get; set; }
+    [JsonIgnore]
     public decimal TotalPrice { get; set; }
+    public string TotalPriceFormatted => TotalPrice.ToString("F2");
     public BookingStatus Status { get; set; }
 }
