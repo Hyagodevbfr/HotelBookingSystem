fd8d93d97cb3e9506473b20e03ba6a6dd2b690b9
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-19 12:51:27 -0300

    feat: adds new information properties in Guest booking dto

--- HotelBookingAPI/Dtos/GuestBookingDto.cs
+++ HotelBookingAPI/Dtos/GuestBookingDto.cs
@@ -1,7 +1,19 @@
-﻿namespace HotelBookingAPI.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelBookingAPI.Dtos;
 
 public class GuestBookingDto
 {
     public int BookingId { get; set; }
     public int GuestId { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string RegistrationId { get; set; } = string.Empty;
+    public string BirthDate { get; set; } = string.Empty;
+
+    public bool HasSpecialNeeds { get; set; }
+    public string? SpecialNeedsDetails { get; set; }
+    public string? DietaryPreferences { get; set; }
+
 }
