f33102bf51f291dd136df7dcc22cf651a11c1041
hyagodevbfr <hyago.sbrito@gmail.com>
2025-03-21 10:31:40 -0300

    feat: add check booking interface

---
+++ HotelBookingAPI/Infra/Data/Repositories/ICheckBooking.cs
@@ -0,0 +1,9 @@
+﻿using HotelBookingAPI.Dtos;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Infra.Data.Repositories;
+
+public interface ICheckBooking
+{
+    Task<BookingDuplicatedRS?> CheckDuplicateBooking(Booking booking);
+}
